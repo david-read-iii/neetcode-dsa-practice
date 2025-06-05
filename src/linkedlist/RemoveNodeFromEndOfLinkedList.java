4ab09456f19fe39ba6d5f5130e9b776e1331332d
David Read <david-read-iii@users.noreply.github.com>
2025-06-05 17:16:26 -0400

    Complete algorithm for Remove Node From End of Linked List.

---
+++ src/linkedlist/RemoveNodeFromEndOfLinkedList.java
@@ -0,0 +1,81 @@
+package linkedlist;
+
+/*
+ * Remove Node From End of Linked List
+ *
+ * You are given the beginning of a linked list head, and an integer n.
+ *
+ * Remove the nth node from the end of the list and return the beginning of the list.
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4], n = 2
+ * Output: [1,2,4]
+ *
+ * Example 2:
+ *
+ * Input: head = [5], n = 1
+ * Output: []
+ *
+ * Example 3:
+ *
+ * Input: head = [1,2], n = 2
+ * Output: [2]
+ *
+ * Constraints:
+ *
+ * 1. The number of nodes in the list is sz.
+ * 2. 1 <= sz <= 30
+ * 3. 0 <= Node.val <= 100
+ * 4. 1 <= n <= sz
+ *
+ * Recommended Time & Space Complexity
+ *
+ * You should aim for a solution with O(N) time and O(1) space, where N is the length of the given list.
+ */
+class RemoveNodeFromEndOfLinkedList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int fastIterations = 0;
+
+        while (!isLastListNode(fast)) {
+            if (fastIterations >= n) {
+                slow = slow.next;
+            }
+            if (fast.next != null) {
+                fast = fast.next;
+            }
+            fastIterations++;
+        }
+
+        // slow is pointing at the node that needs to be repointed.
+        if (fastIterations < n) {
+            return slow.next;
+        } else {
+            slow.next = slow.next.next;
+        }
+
+        return head;
+    }
+
+    public boolean isLastListNode(ListNode node) {
+        return node.next == null;
+    }
+}
+
+class RemoveNodeFromEndOfLinkedListTester {
+
+    public static void printExample(ListNode head, int n, String exampleId) {
+        RemoveNodeFromEndOfLinkedList removeNodeFromEndOfLinkedList = new RemoveNodeFromEndOfLinkedList();
+        System.out.println("removeNthFromEnd" + exampleId + "=" + removeNodeFromEndOfLinkedList.removeNthFromEnd(head, n));
+    }
+
+    public static void main(String[] args) {
+        printExample(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))), 2, "1");
+        printExample(new ListNode(5), 1, "2");
+        printExample(new ListNode(1, new ListNode(2)), 2, "3");
+        printExample(new ListNode(1, new ListNode(2)), 1, "4");
+    }
+}
